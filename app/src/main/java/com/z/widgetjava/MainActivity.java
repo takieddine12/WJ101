d0af78f8a706bd115e5ce098ce61b18f2c7c6d50
takieddine12 <algeriahero22@gmail.com>
2023-11-28 19:01:46 +0100

    new changes

--- app/src/main/java/com/z/widgetjava/MainActivity.java
+++ app/src/main/java/com/z/widgetjava/MainActivity.java
@@ -1,5 +1,6 @@
 package com.z.widgetjava;
 
+import android.Manifest;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.IntentFilter;
@@ -7,17 +8,23 @@ import android.os.Build;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private TelephonyManager telephonyManager;
     private final NetworkChangeReceiver networkChangeReceiver = new NetworkChangeReceiver();
 
+    @RequiresApi(api = Build.VERSION_CODES.TIRAMISU)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS},0);
+        }
 
     }
 
